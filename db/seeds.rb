70a3983725c2cc182b22fc35a6aeeb8245b67030
Rafael Garcia <rafbgarcia@gmail.com>
2013-09-07 20:56:09 -0300

    Added test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: UTF-8
+
+Place.create(name: "Bar do Zé", lat: 1, lng: 1)
+Place.create(name: "Zeca's", lat: 1, lng: 1)
+Place.create(name: "Berimbelo's", lat: 1, lng: 1)
+Place.create(name: "Cerva do Sansão", lat: 1, lng: 1)
