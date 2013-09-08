6d7f42ad524678fdfb014485723b8697d2384228
Rafael Garcia <rafbgarcia@gmail.com>
2013-09-08 11:54:42 -0300

    Updated db seed, included a report for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # encoding: UTF-8
 
-Place.create(name: "Bar do Zé", lat: 1, lng: 1)
-Place.create(name: "Zeca's", lat: 1, lng: 1)
-Place.create(name: "Berimbelo's", lat: 1, lng: 1)
-Place.create(name: "Cerva do Sansão", lat: 1, lng: 1)
+place = Place.where(name: "Bar do Zé", lat: 1, lng: 1).first_or_create
+Place.where(name: "Zeca's", lat: 1, lng: 1).first_or_create
+Place.where(name: "Berimbelo's", lat: 1, lng: 1).first_or_create
+Place.where(name: "Cerva do Sansão", lat: 1, lng: 1).first_or_create
+
+Report.create(place_id: place.id)
